25db373bfa9ffbd0d1f8b7d42d39f99e5cda1f8f
Lansing Nye-Madden <me@lansing.io>
2015-03-03 12:02:01 -0500

    Figure out how modules work (Cont.)

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var test1 = function() {
+	console.log("Test 1: Successful!")
+}
+
+var test2 = function() {
+	console.log("Test 2: Successful!")
+}
+
+module.exports = {
+	testing1: test1,
+	testing2: test2
+};
